c912e48cc49a02245a605a69330565d30b9168f1
Robert <youngr2000-github@yahoo.com>
2023-01-27 16:33:37 +0000

    Initial commit of python todo list CLI application.

---
+++ main.py
@@ -0,0 +1,94 @@
+# Press Shift+F10 to execute
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Starting values:
+todofile = "todos.txt"  # Default todo file path
+todoList = [todofile]
+
+import time
+from todofunctions import getTodoList, write_todos, confirmYN, print_todo, get_userArg
+# Press the green button in the gutter to run the script.
+
+if __name__ == '__main__':
+    print(time.strftime("It is %H:%M on %d %b %Y"))
+    todofile = "todos.txt"  # Default todos file
+    userPrompt = "Enter a ToDo: "
+    actionPrompt = "Type new, open, add, edit, remove, list, save, or quit: "
+#    todoList = [todofile]
+    userAction = "Ask"  # Dummy value to satisfy the condition in the first loop of the while
+    todoList = getTodoList(todofile)  # Get the default todos
+
+    while not userAction == "":  # Exit if userAction is empty
+        userAction = input(actionPrompt).strip().lower()  # Get the next user action
+        userArg = get_userArg(userAction)
+        match userAction[0]:
+            case 'n':
+                todoList = getTodoList(userArg)
+            case 'o':
+                todoList = getTodoList(userArg)
+            case 'a':
+                todo = userArg
+                while todo == "":
+                    todo = input(userPrompt)
+                todoList.append(todo.strip().capitalize() + '\n')
+                print(todoList)
+                write_todos(todoList)
+
+            case 'e':   # edit an item in the list
+                todo = ""
+                if userArg == "":
+                    print_todo(todoList)
+                    try:
+                        num = int(input("\nEnter number of item to replace (0 to exit): "))
+                    except ValueError:
+                        num = 0
+                elif userArg.isdigit():
+                    num = int(userArg)
+                else:
+                    try:
+                        num, todo = userArg.split(" ", 1)
+                        num = int(num)
+                    except ValueError:
+                        num = 0
+                        print("Invalid edit command.  The correct syntax is:")
+                        print("e[dit] [n [todo]]")
+                        print("Where n is the number of an existing todo item to be edited")
+                        print("and todo is the todo to replace that item.")
+
+                if num < len(todoList):
+                    if num > 0 and confirmYN(f'Edit: {num}. {todoList[num]}'):  # Confirm the item to be edited
+                        todo = input(userPrompt).strip().capitalize()  # input the new item
+                        todoList[num] = todo + '\n'  # replace with the new item
+                        write_todos(todoList)
+                else:
+                    print(f"There is no item with that number: ({num}).")
+
+            case 'r':
+                try:
+                    num = int(userArg)
+                except ValueError:
+                    print_todo(todoList)
+                    try:
+                        num = int(input("\nEnter number of item to remove (0 to exit): "))
+                    except ValueError:
+                        num = 0
+                if num < len(todoList):
+                    if num > 0 and confirmYN(f'Remove: {num}. {todoList[num]}'):
+                        print(f'Removed: {num}. {todoList.pop(num)}')
+                        write_todos(todoList)
+                else:
+                    print("There is no item with that number")
+
+            case 'l':
+                print_todo(todoList)
+            case 's':
+                if not userArg == "":  # Save to a new filename
+                    todoList[0] = userArg
+                write_todos(todoList)
+            case 'q':
+                break
+            case _else:
+                pass
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ todofunctions.py
@@ -0,0 +1,68 @@
+TODOFILE = "todos.txt"  # Default todo file path
+TODOLIST = [todofile]
+
+def print_todo(todos=TODOLIST):
+    """Prints the todo list in todoList"""
+    # Use a breakpoint in the code line below to debug your script.
+    message = "\nTo Do List: " + todos[0]
+    print(message)  # Press Ctrl+F8 to toggle the breakpoint.
+    print(len(message) * '-')
+    for i, item in enumerate(todos[1:]):
+        print(f'{i+1}. {item}', end="")
+    print()
+
+
+def get_userArg(userAction=""):  # Return the user argument to a userAction
+    """ Returns the user argument in the string userAction"""
+    try:
+        return userAction.split(" ", 1)[1]
+    except IndexError:
+        return ""
+
+
+def confirmYN(text="Confirm"):
+    """ Asks the user to confirm.  Returns True if the answer is Yes, and False if the answer is no"""
+    while True:
+        confirm = input(text + " (Y/N)? ").strip().lower()
+        try:
+            match confirm[0]:
+                case 'y':
+                    return True
+                case 'n':
+                    return False
+        except:
+            print("Please answer yes or no.")
+            continue
+
+
+def getTodoList(filename=TODOFILE):
+    """ open a todo list file at filename and read the todoList
+        Return the todo list with the filename as the first item """
+    while filename == "":
+        filename = input("Enter filename: ")
+    todos = [filename]
+    try:
+        with open(filename, "r") as file:
+            todos = todos + file.readlines()
+        print_todo(todos)
+        return todos
+    except FileNotFoundError:
+        if confirmYN("New ToDo List," + filename):
+            return todos
+
+
+def write_todos(todos=TODOLIST):
+    """ Write the todos list to a file at path todos[0] """
+    while todos[0] == '':
+        file_name = input("Filename: ")
+        todos[0] = file_name  # Insert new file name
+    try:
+        with open(todos[0], 'w') as file:
+            file.writelines(todos[1:])
+    except:
+        print("Error writing Todo list to file:", todos[0])
+        todos[0] = '\n'
+
+if __name__ == '__main__':
+    print("Hello from todofunctions")
+    print(getTodoList())
